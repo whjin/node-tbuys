7ccafd3392cb17210d41548b007b1df064600b61
whjin <1414848321@qq.com>
2023-05-17 00:00:42 +0800

    update

--- routes/crawler/index.js
+++ routes/crawler/index.js
@@ -6,81 +6,78 @@ const url = require("url");
 
 const imgDir = 'public/crawlerImg';
 
+function start (site, callback) {
+  request(site, async function (err, res, body) {
+    if (err) {
+      return new Error(err);
+    }
+    if (res) {
+      // console.log(JSON.parse(JSON.stringify(res.request.uri)));
+      let list = await findImg(site, body, downLoad);
+      callback(list);
+    }
+  });
+}
+
+function findImg (site, dom, callback) {
+  let $ = cheerio.load(dom);
+  const imgList = [];
+  $('img').each(function (i, elem) {
+    let imgSrc = $(this).attr('src');
+    if (imgSrc.includes(".")) {
+      let srcItem;
+      if (imgSrc.includes("//")) {
+        let protocol = site.split("//").shift();
+        srcItem = imgSrc.startsWith(protocol) ? imgSrc : `${protocol}${imgSrc}`;
+      } else {
+        imgSrc = imgSrc.startsWith("/") ? imgSrc : `/${imgSrc}`;
+        srcItem = site + imgSrc;
+      }
+      let ext = imgSrc.split(".").pop();
+      let filename = `${i}.${ext}`;
+      imgList.push(srcItem);
+      callback(srcItem, filename);
+    }
+  });
+  return imgList;
+}
+
+function downLoad (site, filename) {
+  request(site).pipe(fs.createWriteStream(path.join(imgDir, filename)));
+}
+
 module.exports = function (app) {
   app.get('/crawler', function (req, res) {
-    res.render('crawler', { list: [] });
+    const { type, site } = req.query;
     let Crawler = global.dbHelper.getModel('crawler');
-    console.log(req.query);
-    // Crawler.findOne({})
-    // const site = "https://www.szu.edu.cn";
-    const site = "https://www.scut.edu.cn/new";
-    // const site = "https://www.baidu.com";
-
-    function start (callback) {
-      request(site, async function (err, res, body) {
+    if (type == "reset") {
+      res.render("crawler", { list: [] });
+    } else {
+      Crawler.findOne({}, async function (err, doc) {
         if (err) {
-          return new Error(err);
-        }
-        if (res) {
-          // console.log(JSON.parse(JSON.stringify(res.request.uri)));
-          let list = await findImg(site, body, downLoad);
-          callback(list);
-        }
-      });
-    }
-
-    function findImg (site, dom, callback) {
-      let $ = cheerio.load(dom);
-      const imgList = [];
-      $('img').each(function (i, elem) {
-        let imgSrc = $(this).attr('src');
-        if (imgSrc.includes(".")) {
-          let srcItem;
-          if (imgSrc.includes("//")) {
-            let protocol = site.split("//").shift();
-            srcItem = imgSrc.startsWith(protocol) ? imgSrc : `${protocol}${imgSrc}`;
+          res.sendStatus(500);
+        } else {
+          if (doc) {
+            start(doc.site, async list => {
+              await Crawler.updateOne({}, {
+                $set: {
+                  cSite: doc.site,
+                  cList: list
+                }
+              });
+              res.render("crawler", { list });
+            });
           } else {
-            imgSrc = imgSrc.startsWith("/") ? imgSrc : `/${imgSrc}`;
-            srcItem = site + imgSrc;
+            start(site, async list => {
+              await Crawler.create({
+                cSite: site,
+                cList: list
+              });
+              res.render("crawler", { list });
+            });
           }
-          let ext = imgSrc.split(".").pop();
-          let filename = `${i}.${ext}`;
-          imgList.push(srcItem);
-          callback(srcItem, filename);
         }
       });
-      return imgList;
     }
-
-    function downLoad (url, filename) {
-      // request(url).pipe(fs.createWriteStream(path.join(imgDir, filename)));
-    }
-    // start(list => {
-    //   Crawler.findOne({}, async function (err, doc) {
-    //     if (err) {
-    //       res.sendStatus(500);
-    //     } else {
-    //       const { type } = req.query;
-    //       switch (type) {
-    //         case "start":
-    //           // if (doc) {
-    //           //   await Crawler.updateOne({}, { $set: { cList: list } });
-    //           // } else {
-    //           //   await Crawler.create({ cList: list });
-    //           // }
-    //           // res.render('crawler', { list });
-    //           res.render('crawler', { list: [] });
-    //           break;
-    //         case "reset":
-    //           await Crawler.updateOne({}, { $set: { cList: [] } });
-    //           res.render('crawler', { list: [] });
-    //           break;
-    //         default:
-    //           res.render('crawler', { list });
-    //           break;
-    //       }
-    //     }
-    //   });
-    // });
   });
 };
