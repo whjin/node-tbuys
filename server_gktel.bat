2c36cdd54667c56e5706fd2b705ad5a811fa1741
whjin <1414848321@qq.com>
2023-05-19 14:29:35 +0800

    update

--- server_gktel.bat
+++ server_gktel.bat
@@ -1,5 +1,5 @@
 cd D:\MongoDB\Server\6.0\bin
 start cmd /k mongod --dbpath "D:\MongoDB\Server\6.0\data\tbuysdb"
 
-cd D:\GKTel\Projects\github\node-tbuys
+cd D:\GKTel\WProject\github\node-tbuys
 start cmd /k npm run dev
